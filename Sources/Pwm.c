78dc67b727006751cbb49ca7796d0a794b6cc379
Konstantinos Gkotzamanidis <KGkotzamanidis@users.noreply.github.com>
2018-09-20 17:14:04 +0300

    pulse width modulation
    Create class and libraries for PWM Signal

---
+++ Sources/Pwm.c
@@ -0,0 +1,67 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS.
+ */
+ #include "Pwm.h"
+ 
+ void Pwm_Init(uint8_t Channel){
+	 switch(Channel){
+		 case 0:
+			CCP1CON=0x0F;
+			PR2=100;
+			CCPR1L=50;
+			Pic_BitClear(TRIC,2);
+			break;
+		 case 1:
+			CCP1CON=0x0F;
+			PR2=100;
+			CCPR2L=50;
+			Pic_BitClear(TRIC,1);
+			break;
+	 }
+ }
+ 
+ void Pwm_setDutyCycle(uint8_t Channel, uint8_t DutyCycle){
+	 if(DutyCycle>100){
+		 DutyCycle=100;
+	 }
+	 switch(Channel){
+		 case 0:
+			CCPR1L=DutyCycle;
+			break;
+		case 1:
+			CCPR2L=DutyCycle;
+			break;
+	 }
+ }
+ 
+  void Pwm_Start(uint8_t Channel){
+	  TMR2ON=1;
+  }
+  
+  void Pwm_Stop(uint8_t Channel){
+	  switch(Channel){
+		  case 0:
+			CCP1CON=0x00;
+			break;
+		case 1:
+			CCP2CON=0x00;
+			break;
+	  }
+  }
