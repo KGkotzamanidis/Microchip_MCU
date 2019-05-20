f42e58da52c19b1ecd611fddd2e2f1cdf7bc02e2
konstantinos <konstantinosgkotzamanidis@outlook.com>
2019-05-20 19:55:13 +0300

    Main test program

---
+++ Sources/main.c
@@ -0,0 +1,34 @@
+#pragma config FOSC = INTRC_CLKOUT// Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = OFF         // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include "timer.h"
+#include "lcd.h"
+#include "oscillator.h"
+
+void main(void){
+    InternalClock_Init(8);
+    T2CON = 0b00000100;
+    LCD_SetUp(RC_4,R_NULL,RC_5,R_NULL,R_NULL,R_NULL,R_NULL,RD_0,RD_1,RD_2,RD_3);
+    LCD_Init(2,16);
+    LCD_GoToLine(0);
+    LCD_Clear();
+    
+    LCD_Printf("TMR2 0 to 256:\n%b\n",TMR2);
+    return;
+}
