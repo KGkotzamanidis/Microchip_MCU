10559a28ce11c740b499449b3721a291eae37da1
Konstantinos Gkotzamanidis <KGkotzamanidis@users.noreply.github.com>
2023-12-03 22:48:35 +0200

    Fixing the Interrupt for RB ports

--- Source/Interrupt.c
+++ Source/Interrupt.c
@@ -27,33 +27,41 @@ void Interrupt_enable(bool Peripheral,bool Timer0, bool INT, bool INT_RB, INTERR
         RBIE = 1;
         switch(channel){
             case IOCB_0:
+                pinMode(RB_0,INPUT);
                 IOCB0 = 1;
                 ANSELHbits.ANS12 = 0;
                 break;
             case IOCB_1:
+                pinMode(RB_1,INPUT);
                 IOCB1 = 1;
                 ANSELHbits.ANS10 = 0;
                 break;
             case IOCB_2:
+                pinMode(RB_2,INPUT);
                 IOCB2 = 1;
                 ANSELHbits.ANS8 = 0;
                 break;
             case IOCB_3:
+                pinMode(RB_3,INPUT);
                 IOCB3 = 1;
                 ANSELHbits.ANS9 = 0;
                 break;
             case IOCB_4:
+                pinMode(RB_4,INPUT);
                 IOCB4 = 1;
                 ANSELHbits.ANS11 = 0;
                 break;
             case IOCB_5:
+                pinMode(RB_5,INPUT);
                 IOCB5 = 1;
                 ANSELHbits.ANS13 = 0;
                 break;
             case IOCB_6:
+                pinMode(RB_6,INPUT);
                 IOCB6 = 1;
                 break;
             case IOCB_7:
+                pinMode(RB_7,INPUT);
                 IOCB7 = 1;
                 break;
             case R_NULL:
