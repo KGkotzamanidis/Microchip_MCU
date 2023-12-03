d7b9008c16a51816d5733104a11c271f6b36aafd
Konstantinos Gkotzamanidis <KGkotzamanidis@users.noreply.github.com>
2023-12-04 00:41:27 +0200

    Fixing Interrupts

--- Source/Interrupt.c
+++ Source/Interrupt.c
@@ -16,12 +16,12 @@ void Interrupt_enable(bool Peripheral,bool Timer0, bool INT, bool INT_RB, INTERR
     if(INT){
         pinMode(RB_0,INPUT);
         INTE = 1;
-        ANSELHbits.ANS12 = 0;
+        ANS12 = 0;
         INTEDG = 0;
     }
     else{
         INTE = 0;
-        ANSELHbits.ANS12 = 1;
+        ANS12 = 1;
     }
     if(INT_RB){
         RBIE = 1;
@@ -29,32 +29,32 @@ void Interrupt_enable(bool Peripheral,bool Timer0, bool INT, bool INT_RB, INTERR
             case IOCB_0:
                 pinMode(RB_0,INPUT);
                 IOCB0 = 1;
-                ANSELHbits.ANS12 = 0;
+                ANS12 = 0;
                 break;
             case IOCB_1:
                 pinMode(RB_1,INPUT);
                 IOCB1 = 1;
-                ANSELHbits.ANS10 = 0;
+                ANS10 = 0;
                 break;
             case IOCB_2:
                 pinMode(RB_2,INPUT);
                 IOCB2 = 1;
-                ANSELHbits.ANS8 = 0;
+                ANS8 = 0;
                 break;
             case IOCB_3:
                 pinMode(RB_3,INPUT);
                 IOCB3 = 1;
-                ANSELHbits.ANS9 = 0;
+                ANS9 = 0;
                 break;
             case IOCB_4:
                 pinMode(RB_4,INPUT);
                 IOCB4 = 1;
-                ANSELHbits.ANS11 = 0;
+                ANS11 = 0;
                 break;
             case IOCB_5:
                 pinMode(RB_5,INPUT);
                 IOCB5 = 1;
-                ANSELHbits.ANS13 = 0;
+                ANS13 = 0;
                 break;
             case IOCB_6:
                 pinMode(RB_6,INPUT);
